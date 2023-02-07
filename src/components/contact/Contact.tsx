69d1382c3331c94f79acd1fd2a39082fed9a8bfa
mattkcw <mattkcw@outlook.com>
2023-02-07 04:18:15 -0800

    updated button

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -108,7 +108,7 @@ const Contact = () => {
               className="p-5 bg-yellow font-semibold text-deep-blue mt-5 hover:bg-red hover:text-white transition duration-500"
               type="submit"
             >
-              SEND ME A MESSAGE
+              SEND MESSAGE   
             </button>
           </form>
         </motion.div>
